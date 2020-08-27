99855886c00677723288cfca0abde532f15099f0
sleypneer <sleypneer@mail.ru>
2020-08-27 14:24:16 +0300

    added ignore and report

---
+++ src/main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class main {
+    public static void main (String [] agrs) {
+        int score=2_000_000_000;
+        int transfer=500_000_000;
+        int total=score*transfer;
+
+        System.out.println(total);
+
+    }
+}
